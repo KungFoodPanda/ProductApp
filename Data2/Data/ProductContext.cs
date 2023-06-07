b76b007d85b733589439155bd78d07eed7584d50
KungFoodPanda <122403442+KungFoodPanda@users.noreply.github.com>
2023-06-07 12:05:03 +0300

    Add files via upload

---
+++ Data2/Data/ProductContext.cs
@@ -0,0 +1,20 @@
+﻿using Data2.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data2.Data
+{
+    public class ProductContext:DbContext
+
+    {
+        public ProductContext():base("name=ProductContext")
+        {
+            
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
