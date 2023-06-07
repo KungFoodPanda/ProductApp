f09d99cace6d41f357f308d7c8fbd9f8941a7ddc
pc <pc@DESKTOP-29NH46L>
2023-06-07 12:01:47 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace ProductApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtStock = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.lblStock = new System.Windows.Forms.Label();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(130, 63);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(124, 20);
+            this.txtName.TabIndex = 0;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(130, 132);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(124, 20);
+            this.txtPrice.TabIndex = 1;
+            // 
+            // txtStock
+            // 
+            this.txtStock.Location = new System.Drawing.Point(130, 196);
+            this.txtStock.Name = "txtStock";
+            this.txtStock.Size = new System.Drawing.Size(124, 20);
+            this.txtStock.TabIndex = 2;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(47, 70);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(38, 13);
+            this.lblName.TabIndex = 3;
+            this.lblName.Text = "Name:";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(47, 135);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(34, 13);
+            this.lblPrice.TabIndex = 4;
+            this.lblPrice.Text = "Price:";
+            // 
+            // lblStock
+            // 
+            this.lblStock.AutoSize = true;
+            this.lblStock.Location = new System.Drawing.Point(47, 203);
+            this.lblStock.Name = "lblStock";
+            this.lblStock.Size = new System.Drawing.Size(38, 13);
+            this.lblStock.TabIndex = 5;
+            this.lblStock.Text = "Stock:";
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(50, 262);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 6;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(169, 261);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(293, 260);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(169, 311);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Visible = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(388, 63);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(400, 271);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.lblStock);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtStock);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtStock;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label lblStock;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,141 @@
+﻿using Business;
+using Data2.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProductApp
+{
+    
+    public partial class Form1 : Form
+    {
+        private ProductBusiness productBusiness = new ProductBusiness();
+        private int editId = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UpdateGrid();
+            ClearTextBoxes();
+        }
+        private void UpdateGrid()
+        {
+            dataGridView1.DataSource = productBusiness.GetAll();
+            dataGridView1.ReadOnly = true;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        }
+
+        private void ClearTextBoxes()
+        {
+            txtName.Text = "";
+            txtPrice.Text = "0";
+            txtStock.Text = "0";
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            var name = txtName.Text;
+            decimal price = 0;
+            decimal.TryParse(txtPrice.Text, out price);
+            int stock = 0;
+            int.TryParse(txtStock.Text, out stock);
+
+            Product product = new Product();
+            product.Name = name;
+            product.Price = price;
+            product.Stock = stock;
+
+            productBusiness.Add(product);
+            UpdateGrid();
+            ClearTextBoxes();
+        }
+
+        private void UpdateTextBoxes(int id)
+        {
+            Product update = productBusiness.Get(id);
+            txtName.Text = update.Name;
+            txtPrice.Text=update.Price.ToString();
+            txtStock.Text = update.Stock.ToString();
+        }
+
+        private void ToggleSaveUpdate()
+        {
+            if(btnUpdate.Visible)
+            {
+                btnSave.Visible = true;
+                btnUpdate.Visible = false;
+            }
+            else { btnSave.Visible = false;
+            btnUpdate.Visible = true;}
+        }
+
+        private void DisableSelect()
+        {
+            dataGridView1.Enabled = false;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                var item = dataGridView1.SelectedRows[0].Cells;
+                var id = int.Parse(item[0].Value.ToString());
+                editId = id;
+                UpdateTextBoxes(id);
+                ToggleSaveUpdate();
+                DisableSelect();
+            }
+        }
+
+        private Product GetEditedProduct()
+        {
+            Product product = new Product();
+            product.Id = editId;
+
+            var name = txtName.Text;
+            decimal price = 0;
+            decimal.TryParse(txtPrice.Text, out price);
+            int stock = 0;
+            int.TryParse(txtStock.Text, out stock);
+            product.Name = name;
+            product.Price = price;
+            product.Stock = stock;
+
+            return product;
+        }
+
+        private void ResetSelect()
+        {
+            dataGridView1.ClearSelection();
+            dataGridView1.Enabled = true;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Product editedProduct = GetEditedProduct();
+            productBusiness.Update(editedProduct);
+            UpdateGrid();
+            ResetSelect();
+            ToggleSaveUpdate();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            var item = dataGridView1.SelectedRows[0].Cells;
+            var id = int.Parse(item[0].Value.ToString()) ;
+            productBusiness.Delete(id);
+            UpdateGrid();
+            ResetSelect();
+        }
+    }
+}
